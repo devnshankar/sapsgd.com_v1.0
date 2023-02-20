42af2238b1e6db88041cc45a0898be6fefe19270
Subha shankar Behera <100505729+subhankar496k@users.noreply.github.com>
2023-02-20 23:47:29 +0530

    added all social links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "../components/styles/Footer.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faInstagram, faLinkedin, faWhatsapp, faTelegram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
@@ -13,12 +13,10 @@ function Footer() {
         <Link to="/contact" className='nav-link'>Contact</Link>
       </div>
       <div className="footer-socials">
-        <a href="#" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebookF} /></a>
-        <a href="#" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
-        <a href="#" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
-        <a href="#" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
-        <a href="#" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faWhatsapp} /></a>
-        <a href="#" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTelegram} /></a>
+        <a href="https://www.facebook.com/profile.php?id=100090250130445" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebookF} /></a>
+        <a href="https://twitter.com/sapsgd" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
+        <a href="https://www.instagram.com/sapsgdnilgiri/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+        <a href="https://www.linkedin.com/in/sapsgd-nilgiri" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
       </div>
       <p className="footer-text">
         &copy; 2023 sapsgd.com . All Rights Reserved.
