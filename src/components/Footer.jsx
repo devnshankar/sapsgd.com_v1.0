ceb6cd34fad63a9acf595355c0f89a16819e2565
Subha shankar Behera <100505729+subhankar496k@users.noreply.github.com>
2023-02-22 10:26:51 +0530

    commented the linkedIn button due to broken profile link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import "../components/styles/Footer.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faTwitter, faInstagram, /*faLinkedin*/ } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
@@ -16,7 +16,7 @@ function Footer() {
         <a href="https://www.facebook.com/profile.php?id=100090250130445" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebookF} /></a>
         <a href="https://twitter.com/sapsgd" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
         <a href="https://www.instagram.com/sapsgdnilgiri/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
-        <a href="https://www.linkedin.com/in/sapsgd-nilgiri" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+        {/*<a href="https://www.linkedin.com/in/sapsgd-nilgiri" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>*/}
       </div>
       <p className="footer-text">
         &copy; 2023 sapsgd.com . All Rights Reserved.
