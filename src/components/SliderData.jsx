01e9ec45bcf1b2ed69d4cfded982d0756ba6d128
Subha shankar Behera <100505729+subhankar496k@users.noreply.github.com>
2023-02-23 01:57:31 +0530

    added a gallery slider feature to the Gallery page

---
+++ src/components/SliderData.jsx
@@ -0,0 +1,20 @@
+
+import image1 from "../assets/images/schoolpic.jpg";
+import image2 from "../assets/images/schoolpic1.jpg";
+import image3 from "../assets/images/schoolpic2.jpg";
+import image4 from "../assets/images/schoolpic3.jpg";
+
+export const SliderData = [
+  {
+    image: image1
+  },
+  {
+    image: image2
+  },
+  {
+    image: image3
+  },
+  {
+    image: image4
+  },
+];
--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,19 +1,14 @@
 import "../pages/styles/GalleryPage.scss"
 import React from "react"
-import img1 from "../assets/images/schoolpic.jpg"
-import img2 from "../assets/images/schoolpic1.jpg"
-import img3 from "../assets/images/schoolpic2.jpg"
-import img4 from "../assets/images/schoolpic3.jpg"
+import ImageSlider from "../components/ImageSlider"
+import { SliderData } from "../components/SliderData"
 
 
 function GalleryPage() {
 
   return (
-    <div className="gallery">
-      <img src={img1} alt=''/>
-      <img src={img2} alt=''/>
-      <img src={img3} alt=''/>
-      <img src={img4} alt=''/>
+    <div className="gallerypage">
+      <ImageSlider slides={SliderData}/>
     </div>
   )
 }
