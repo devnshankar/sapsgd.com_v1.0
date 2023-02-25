c67ccc85cc78643b073351b3dad6fc7ca990367a
Subha shankar Behera <100505729+subhankar496k@users.noreply.github.com>
2023-02-25 16:01:23 +0530

    home page feature complete

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "../pages/styles/HomePage.scss"
 
 
@@ -6,29 +7,47 @@ import "../pages/styles/HomePage.scss"
 function HomePage() {
   return (
     <div className='homepage'>
-      <p>
-      Shree Aurobindo Purnanga Siksha Gabeshana Dham is a unique school based on the philosophy and teachings of Sri Aurobindo and The Mother. The school is located in a serene and picturesque environment that provides a peaceful and conducive atmosphere for learning.
+      <p className='home-text'>
+        <header className='home-header'>
+		      <h1>Welcome to Shree Aurobindo Purnanga Siksha Gabeshana Dham</h1>
+		      <h2>Preparing Students for the Future</h2>
+	      </header>
 
-At Shree Aurobindo Purnanga Siksha Gabeshana Dham, the emphasis is on holistic education that nurtures the physical, mental, emotional, and spiritual well-being of the students. The school is committed to providing a comprehensive education that equips students with the knowledge, skills, and values required to lead successful and fulfilling lives.
+	      <main className='home-main'>
+		      <section>
+			      <h3>About Us</h3>
+			      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Shree Aurobindo Purnanga Siksha Gabeshana Dham is a school that is blessed by The Mother and Shree Aurobindo. This educational institution is founded on the principles of Integral Education, which integrates the spiritual and material dimensions of life. The school's name itself is a testament to its philosophy, as "Purnanga" means the complete or integral development of a person, and "Siksha Gabeshana Dham" means the place where education is explored and researched.</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;At this school, spirituality is not just a peripheral aspect of education, but rather, it is an integral and foundational part of the curriculum. The aim of the school is to provide a holistic education that develops the physical, emotional, mental, and spiritual aspects of the individual. The school recognizes that true education goes beyond the mere accumulation of knowledge, and that it must also cultivate a deeper understanding of the self and the world. <Link to="/about" className='home-link'>more.</Link></p>
+		      </section>
 
-The school's curriculum is designed to provide a balanced education that includes academics, arts, sports, and co-curricular activities. The academic program is based on the CBSE (Central Board of Secondary Education) curriculum and is taught by highly qualified and experienced teachers. The school also offers a range of extracurricular activities that include music, dance, drama, art, and sports.
+		      <section>
+			      <h3>Our Programs</h3>
+			      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We offer a comprehensive curriculum that includes STEM, humanities, and the arts. Our students also have the opportunity to participate in extracurricular activities such as robotics club, debate team, and theater productions. <Link to="/academics" className='home-link'>more.</Link></p>
+		      </section>
 
-The school's approach to education is rooted in the teachings of Sri Aurobindo and The Mother, who believed in the integral development of the human being. The school aims to help students discover their true potential and develop a deep sense of self-awareness and spirituality.
+		      <section>
+			      <h3>Admissions</h3>
+			      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If you are interested in applying to ABC School, please visit our <Link to="/admission" className='home-link'>admission</Link> page for more information. We welcome students from all backgrounds and strive to create a diverse and inclusive community.</p>
+		      </section>
 
-One of the unique features of Shree Aurobindo Purnanga Siksha Gabeshana Dham is the emphasis on meditation and yoga. The school believes that these practices are essential for the holistic development of the students and helps them to develop a calm and focused mind. The school has a well-equipped meditation hall and yoga studio where students can practice these techniques under the guidance of trained instructors.
+          <section>
+			      <h3>Administration</h3>
+			      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;To ensure the smooth functioning of the school, a dedicated team of administrators is responsible for overseeing various aspects of its operations. The administration of the Shree Aurobindo Purnanga Siksha Gabeshana Dham is headed by a principal, who is responsible for the overall management of the school. The principal is assisted by a team of coordinators, who oversee different departments and ensure that they are functioning effectively. These departments include academics, sports, extracurricular activities, and discipline. <Link to="/administration" className='home-link'>more.</Link></p>
+		      </section>
 
-Another distinctive aspect of the school is the emphasis on community service and social responsibility. The school encourages students to participate in various community service projects and take an active role in addressing social issues. This helps students to develop a sense of empathy and compassion towards others and become responsible citizens.
-
-The school's faculty is committed to providing a nurturing and supportive environment for the students. They are highly qualified and experienced professionals who are dedicated to the holistic development of the students. The faculty members work closely with the students and provide individual attention and support to help them achieve their full potential.
-
-The school also has a strong parent-teacher association that works in close collaboration with the school management to ensure the smooth functioning of the school. The association plays an active role in organizing various events and activities that help to strengthen the school's community and foster a sense of belonging among the students, parents, and staff.
-
-Shree Aurobindo Purnanga Siksha Gabeshana Dham also offers a range of facilities that include a well-stocked library, science and computer labs, music and dance studios, sports facilities, and a cafeteria that serves healthy and nutritious meals.
-
-The school has a strong commitment to environmental sustainability and has implemented several eco-friendly initiatives. These include rainwater harvesting, waste management, and the use of renewable energy sources. The school also has a garden where students can learn about organic farming and the importance of sustainable agriculture.
-
-In conclusion, Shree Aurobindo Purnanga Siksha Gabeshana Dham is a unique school that offers a holistic education based on the teachings of Sri Aurobindo and The Mother. The school provides a nurturing and supportive environment that helps students to develop their full potential and become responsible and compassionate citizens. With its emphasis on meditation, yoga, community service, and environmental sustainability, Shree Aurobindo Purnanga Siksha Gabeshana Dham is a school that truly embodies the ideals of Sri Aurobindo and The Mother.
+		      <section>
+			      <h3>Contact Us</h3>
+			      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Feel free to reach out to us if you have any questions or would like to schedule a visit. You can contact us by phone or email, or visit our campus in person.</p>
+            <ul>
+				      <li>Phone: 123-456-7890</li>
+				      <li><a className='home-mail' href="https://mail.google.com/mail/?view=cm&to=sapsgdofficial@gmail.com&su=Hello!!!&body=">sapsgdofficial@gmail.com</a></li>
+				      <li>Address: Nilgiri, Balasore, Odisha</li>
+              <li><Link to="/contact" className='home-link'>more.</Link></li>
+			      </ul>
+		      </section>
+	      </main>
       </p>
+      
     </div>
   )
 }
